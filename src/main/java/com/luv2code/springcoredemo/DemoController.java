718c0486174795e9bb324f3a524546d5ff354490
Titus Lim <tituslhy@gmail.com>
2025-11-28 10:29:46 +0800

    initial commit

---
+++ src/main/java/com/luv2code/springcoredemo/DemoController.java
@@ -0,0 +1,24 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private Coach myCoach;
+
+    /**
+     * `Autowired` is optional if you only have one constructor
+     * @param myCoach
+     */
+    @Autowired
+    public DemoController(Coach myCoach) {
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
